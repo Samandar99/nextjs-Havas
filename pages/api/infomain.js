cc72bf5e3977a77b76402e7de11a6936d37ff05d
Samandar99 <laziz.go99@gmail.com>
2024-01-14 08:25:24 +0500

    ss

--- pages/api/infomain.js
+++ pages/api/infomain.js
@@ -2,28 +2,24 @@ import Cors from "micro-cors";
 
 
 const cors = Cors({
-  allowMethods: ["PUT"],
+  allowMethods: ['PUT'],
 });
 
 async function handlerText(req, res) {
-  
-  if(req.method === 'PUT'){
-    const {title, text} = req.body 
-     
+
+  if (req.method === 'PUT') {
+    const { title, text } = req.body
+
     res.status(200).json({ title, text });
-   
+
   }
 
-
-
-
-  
   res.status(200).json({
     title: "О НАС",
     text: "HAVAS - сеть дискаунтеров у дома. Мы предлагаем нашим покупателям качественные продукты по выгодной цене. В наших магазинах представлены товары известных мировых и локальных брендов, а также товары собственного производства под торговой маркой HAVAS",
   });
 
-  
+
 }
 
 export default cors(handlerText);
